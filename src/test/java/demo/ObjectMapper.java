57e43e8b67d9a07700235be126d295be8ada5af7
Umar Shaik mohammad <umarshaikmohammad@Umars-MacBook-Air.local>
2024-07-13 19:29:02 +0530

    ME_QA_XSCRAPE_DATA_MODULE_L1

---
+++ src/test/java/demo/ObjectMapper.java
@@ -0,0 +1,14 @@
+package demo;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+
+public class ObjectMapper {
+
+    public void writeValue(File file, List<HashMap<String, String>> results) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeValue'");
+    }
+
+}
